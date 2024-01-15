770014620d46486b1647f6172a2b70b0174c9621
Tung Quan Hoang <hoangtungquan22@gmail.com>
2024-01-16 08:19:37 +1100

    separate search bar, add functionality to navigate to a pokemon page after search with basic details

--- src/components/PokemonPage.jsx
+++ src/components/PokemonPage.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import SearchBar from './SearchBar';
 
 export default function PokemonPage() {
   const [pokemon, setPokemon] = useState(null);
+  const [species, setSpecies] = useState(null);
   const { pokemonName } = useParams();
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
@@ -17,20 +21,58 @@ export default function PokemonPage() {
       });
   }, [pokemonName]);
 
+  useEffect(() => {
+    if (!pokemon) return;
+    axios.get(pokemon.species.url)
+      .then((res) => {
+        setSpecies(res.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching Pokémon species data:', error);
+      });
+  }, [pokemon]);
+
+  const handleGoHomepage = () => {
+    navigate('/*');
+  };
+
   if (!pokemon) return 'Loading...';
+  if (!species) return 'Loading...';
 
   const { sprites } = pokemon;
 
+  const hatchSteps = (species.hatch_counter + 1) * 255;
+
   return (
-    <div className="pokemon" key={pokemon.id}>
-      <div className="pokemon-id-name">
-        {pokemon.id}
-        {' - '}
-        {pokemon.name}
-      </div>
-      <div className="pokemon-sprites">
-        {sprites.front_default && <img src={sprites.front_default} alt={pokemon.name} />}
-        {sprites.front_shiny && <img src={sprites.front_shiny} alt={pokemon.name} />}
+    <div>
+      <SearchBar />
+      <button type="button" onClick={handleGoHomepage}>
+        Home
+      </button>
+      <div className="pokemon" key={pokemon.id}>
+        <div className="pokemon-id-name">
+          {pokemon.id}
+          {' - '}
+          {pokemon.name}
+        </div>
+        <div className="pokemon-sprites">
+          {sprites.front_default && <img src={sprites.front_default} alt={pokemon.name} />}
+          {sprites.front_shiny && <img src={sprites.front_shiny} alt={pokemon.name} />}
+        </div>
+        <div className="species-details">
+          <strong>Capture rate: </strong>
+          {species.capture_rate}
+          <br />
+          <strong>Base happiness: </strong>
+          {species.base_happiness}
+          <br />
+          <strong>Hatch counter: </strong>
+          {species.hatch_counter}
+          &nbsp;(
+          {hatchSteps}
+          &nbsp;steps)
+          <br />
+        </div>
       </div>
     </div>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const navigate = useNavigate();
+
+  // Function to handle changes in the pokemon search bar
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  // Function to handle form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate(`/pokemon/${searchQuery.toLowerCase()}`);
+  };
+
   return (
-    <div>SearchBar</div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter Pokémon name"
+        value={searchQuery}
+        onChange={handleSearchChange}
+      />
+      <button type="submit">Search</button>
+    </form>
   );
 }
