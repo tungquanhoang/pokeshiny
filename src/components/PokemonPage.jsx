00e92ee9cf13a4c076128938c435b8a7660daaf6
Tung Quan Hoang <hoangtungquan22@gmail.com>
2024-01-16 09:16:06 +1100

    add pokemon type icons and address an error when a pokemon cannot be searched

--- src/components/PokemonPage.jsx
+++ src/components/PokemonPage.jsx
@@ -3,10 +3,31 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 import SearchBar from './SearchBar';
+import './PokemonPage.css';
+
+import normal from '../assets/type_icons/normal.png';
+import fighting from '../assets/type_icons/fighting.png';
+import flying from '../assets/type_icons/flying.png';
+import poison from '../assets/type_icons/poison.png';
+import ground from '../assets/type_icons/ground.png';
+import rock from '../assets/type_icons/rock.png';
+import bug from '../assets/type_icons/bug.png';
+import ghost from '../assets/type_icons/ghost.png';
+import steel from '../assets/type_icons/steel.png';
+import fire from '../assets/type_icons/fire.png';
+import water from '../assets/type_icons/water.png';
+import grass from '../assets/type_icons/grass.png';
+import electric from '../assets/type_icons/electric.png';
+import psychic from '../assets/type_icons/psychic.png';
+import ice from '../assets/type_icons/ice.png';
+import dragon from '../assets/type_icons/dragon.png';
+import dark from '../assets/type_icons/dark.png';
+import fairy from '../assets/type_icons/fairy.png';
 
 export default function PokemonPage() {
   const [pokemon, setPokemon] = useState(null);
   const [species, setSpecies] = useState(null);
+  const [currentError, setCurrentError] = useState('');
   const { pokemonName } = useParams();
 
   const navigate = useNavigate();
@@ -18,6 +39,7 @@ export default function PokemonPage() {
       })
       .catch((error) => {
         console.error('Error fetching Pokémon data:', error);
+        setCurrentError('Error fetching Pokémon data!');
       });
   }, [pokemonName]);
 
@@ -29,17 +51,77 @@ export default function PokemonPage() {
       })
       .catch((error) => {
         console.error('Error fetching Pokémon species data:', error);
+        setCurrentError('Error fetching Pokémon species data!');
       });
   }, [pokemon]);
 
-  const handleGoHomepage = () => {
+  function handleGoHomepage() {
     navigate('/*');
-  };
+  }
 
-  if (!pokemon) return 'Loading...';
-  if (!species) return 'Loading...';
+  function returnTypeIcon(type) {
+    switch (type) {
+      case 'normal':
+        return normal;
+      case 'fighting':
+        return fighting;
+      case 'flying':
+        return flying;
+      case 'poison':
+        return poison;
+      case 'ground':
+        return ground;
+      case 'rock':
+        return rock;
+      case 'bug':
+        return bug;
+      case 'ghost':
+        return ghost;
+      case 'steel':
+        return steel;
+      case 'fire':
+        return fire;
+      case 'water':
+        return water;
+      case 'grass':
+        return grass;
+      case 'electric':
+        return electric;
+      case 'psychic':
+        return psychic;
+      case 'ice':
+        return ice;
+      case 'dragon':
+        return dragon;
+      case 'dark':
+        return dark;
+      case 'fairy':
+        return fairy;
+      default:
+        return null;
+    }
+  }
+
+  if (currentError) {
+    return (
+      <div>
+        <SearchBar />
+        <button type="button" onClick={handleGoHomepage}>
+          Home
+        </button>
+        <div className="pokemon-error">
+          {currentError}
+        </div>
+      </div>
+    );
+  }
+
+  if (!pokemon && !currentError) return 'Loading...';
+  if (!species && !currentError) return 'Loading...';
 
   const { sprites } = pokemon;
+
+  const types = pokemon.types.map((type) => type.type.name);
 
   const hatchSteps = (species.hatch_counter + 1) * 255;
 
@@ -54,6 +136,11 @@ export default function PokemonPage() {
           {pokemon.id}
           {' - '}
           {pokemon.name}
+        </div>
+        <div className="poke-types">
+          {types.map((type) => (
+            <img src={returnTypeIcon(type)} alt="type" />
+          ))}
         </div>
         <div className="pokemon-sprites">
           {sprites.front_default && <img src={sprites.front_default} alt={pokemon.name} />}
